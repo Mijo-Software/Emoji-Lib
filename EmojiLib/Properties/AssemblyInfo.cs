a88ecb4deb3f90bb4ba456f61ea50628f1626350
Michael Johne <mjohne@users.noreply.github.com>
2020-05-10 06:20:34 +0200

    Add files via upload

--- EmojiLib/Properties/AssemblyInfo.cs
+++ EmojiLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3.9")]
-[assembly: AssemblyFileVersion("0.3.3.9")]
+[assembly: AssemblyVersion("0.4.0.10")]
+[assembly: AssemblyFileVersion("0.4.0.10")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
