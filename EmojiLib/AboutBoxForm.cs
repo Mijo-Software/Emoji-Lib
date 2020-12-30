e20f6e2a4f49efa2c82f664553357240ac7fed6d
Michael Johne <mjohne@users.noreply.github.com>
2020-12-30 21:46:22 +0100

    Add files via upload

--- EmojiLib/AboutBoxForm.cs
+++ EmojiLib/AboutBoxForm.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
+using MijoSoftware.AssemblyInformation;
 
 namespace EmojiLib
 {
@@ -9,86 +10,12 @@ namespace EmojiLib
 		public AboutBoxForm()
 		{
 			InitializeComponent();
-			Text = $"Info über {AssemblyTitle}";
-			labelProductName.Text = AssemblyProduct;
-			labelVersion.Text = $"Version {AssemblyVersion}";
-			labelCopyright.Text = AssemblyCopyright;
-			labelCompanyName.Text = AssemblyCompany;
-			textBoxDescription.Text = AssemblyDescription;
+			Text = $"About {AssemblyInfo.AssemblyTitle}";
+			labelProductName.Text = AssemblyInfo.AssemblyProduct;
+			labelVersion.Text = $"Version {AssemblyInfo.AssemblyVersion}";
+			labelCopyright.Text = AssemblyInfo.AssemblyCopyright;
+			labelCompanyName.Text = AssemblyInfo.AssemblyCompany;
+			textBoxDescription.Text = AssemblyInfo.AssemblyDescription;
 		}
-
-		#region Assemblyattributaccessoren
-
-		public static string AssemblyTitle
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyTitleAttribute), inherit: false);
-				if (attributes.Length > 0)
-				{
-					AssemblyTitleAttribute titleAttribute = attributes[0] as AssemblyTitleAttribute;
-					if (!string.IsNullOrEmpty(value: titleAttribute.Title))
-					{
-						return titleAttribute.Title;
-					}
-				}
-				return Path.GetFileNameWithoutExtension(path: Assembly.GetExecutingAssembly().CodeBase);
-			}
-		}
-
-		public static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
-
-		public static string AssemblyDescription
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyDescriptionAttribute), inherit: false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return (attributes[0] as AssemblyDescriptionAttribute)?.Description;
-			}
-		}
-
-		public static string AssemblyProduct
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyProductAttribute), inherit: false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return (attributes[0] as AssemblyProductAttribute)?.Product;
-			}
-		}
-
-		public static string AssemblyCopyright
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyCopyrightAttribute), inherit: false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return (attributes[0] as AssemblyCopyrightAttribute)?.Copyright;
-			}
-		}
-
-		public static string AssemblyCompany
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyCompanyAttribute), inherit: false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return (attributes[0] as AssemblyCompanyAttribute)?.Company;
-			}
-		}
-		#endregion
 	}
 }
